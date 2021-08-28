bd1ac1dac826ae9d065b8d6fe8430cfad895ec81
ansuleymanova <suleymanova.dev@gmail.com>
2021-08-29 02:31:34 +0300

    add workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'm=bh1wgm6s6ag)f$zf$g@_^%wv_&thwpzumglv4^0(pqibx%r&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
